8c0a6e0c952963b15f5bcd48dcc4fab7a2f9e4f4
Jessica Lam <jemlam93@gmail.com>
2020-10-07 19:01:06 -0700

    still working on product_of_all_other_numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,12 +2,36 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+import math
+
 def product_of_all_other_numbers(arr):
     # Your code here
+    prod = []
+    for i in range(0, len(arr)):
+        if i == 0:
+            first = math.prod(arr[1:])
+            prod.append(first)
+        else:
+            first_half = math.prod(arr[0: i])
+            second_half = math.prod(arr[ i+1 : ])
+            prod.append(first_half)
+            prod.append(second_half)
+    return prod
+    
 
-    pass
-
-
+"""
+For example, given 
+```
+[1, 7, 3, 4]
+```
+your function should return 
+```
+[84, 12, 28, 21]
+``` 
+by calculating 
+```
+[7*3*4, 1*3*4, 1*7*4, 1*7*3]
+"""
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
